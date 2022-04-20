22cf9a35de06a34b6d1d0302d9d3c39d069bf960
Juan Williman <juanwilliman@yahoo.com>
2022-04-20 19:47:03 -0300

    Initial Project with App Icon

---
+++ SpotArtists/SpotArtists/SpotArtistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotArtistsApp.swift
+//  SpotArtists
+//
+//  Created by Juan Williman on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SpotArtistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
